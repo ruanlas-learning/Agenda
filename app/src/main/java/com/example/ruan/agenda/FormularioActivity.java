c4eb0d7b1e752286e631cefd0093ee90681daa45
Ruan Silva <ruanluiz.as@gmail.com>
2018-12-27 13:41:26 -0200

    aula 03

--- app/src/main/java/com/example/ruan/agenda/FormularioActivity.java
+++ app/src/main/java/com/example/ruan/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package com.example.ruan.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,12 +16,35 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button btn_formulario_salvar = (Button) findViewById(R.id.btn_formulario_salvar);
-        btn_formulario_salvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(FormularioActivity.this, "Botão clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        Button btn_formulario_salvar = (Button) findViewById(R.id.btn_formulario_salvar);
+//        btn_formulario_salvar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
+//                finish();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        switch (itemId){
+            case R.id.menu_formulario_ok:
+                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/ruan/agenda/ListaAlunosActivity.java
+++ app/src/main/java/com/example/ruan/agenda/ListaAlunosActivity.java
@@ -1,5 +1,6 @@
 package com.example.ruan.agenda;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -22,6 +24,15 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView lista_alunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
         lista_alunos.setAdapter(adapter);
+
+        Button btn_novo_aluno = (Button)findViewById(R.id.btn_novo_aluno);
+        btn_novo_aluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
